b42fb699bf6de6d79607c495e17fa7531cc0b2e3
MattéoWarmée <102968421+MatWarm@users.noreply.github.com>
2024-07-24 16:09:07 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('database', 'user', 'pwd', {
-  host: '192.168.1.113',
+  host: 'localhost',
   dialect: 'mysql', // or 'sqlite', 'postgres', 'mssql'
   // Additional options like pool configuration could be here
 });
