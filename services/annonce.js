275d8a338c282a24ffe513c832c3d709ddf01637
Pierre Perdigues <pierre.perdigues@gmail.com>
2024-07-26 11:06:29 +0200

    Ajout fonctionnalité dans reservation

--- services/annonce.js
+++ services/annonce.js
@@ -65,14 +65,6 @@ class AnnonceService{
         }
     }
 
-    async getAllAnnonces(){
-        try{
-            const annonces = await annonce.findAll();
-            return annonces;
-        }catch(error){
-            throw new Error(error.message);
-        }
-    }
 }
 
 module.exports = new AnnonceService();
--- services/reservation.js
+++ services/reservation.js
@@ -1,4 +1,5 @@
 const Reservation = require('../models/reservation');
+const Annonce = require('../models/annonce');
 
 class ReservationService{
 
@@ -9,6 +10,20 @@ class ReservationService{
                   id_annonce: id
                 },
                 attributes: ['id', 'date_debut', 'date_fin']
+              });
+              return reservations;          
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+    
+    async getReservationByProfilId(id) {
+        try {
+            const reservations = await Reservation.findAll({
+                where: {
+                  id_compte: id
+                },
+                include: [{ model: Annonce, attributes: ['titre', 'prix_location'] }]
               });
               return reservations;          
         } catch (error) {
